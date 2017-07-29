a22217462e57464963117a592e9cf84e065f37a6
brett <admin@krux.us>
2017-07-28 21:37:44 -0400

    Updated to make use of switch/case statement

--- prices.js
+++ prices.js
@@ -1,20 +1,26 @@
 const cointicker=require("coin-ticker")
 var sprintf=require("sprintf-js").sprintf
 
-res=process.argv[2]+"_BTC"
-sym="฿"
-
 function ticker(exchange,res){
-	if (res === "BTC_USD") sym="$"
+	sym="฿"
+	if (res === "BTC_USD" || res === "USDT_BTC" || res === "USD_BTC") sym="$"
 	cointicker(exchange,res)
 		.then((tick)=>{
 		if(tick.last === undefined) return 1
 		console.log(sprintf("%-10s: %s%f",exchange.toUpperCase(),sym,tick.last))})
 }
 
-
-if (process.argv[2].toUpperCase() === "BTC"){
-	res="BTC_USD"
+switch(process.argv[2].toUpperCase()){
+	case "BTC": 
+		res="BTC_USD"
+		break
+	case "USD": 
+		res="USD_BTC"
+		break
+	case "USDT": 
+		res="USDT_BTC"
+		break
+	default: res=process.argv[2]+"_BTC"
 }
 
 exchange = ["bittrex","poloniex","kraken","okcoin","bitstamp","coinbase"]
